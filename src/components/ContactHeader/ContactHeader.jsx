96c4bff6909b37b942efe7a900764e0740df775f
pradeepmali123 <pradeep.mali@sakec.ac.in>
2025-05-22 09:40:31 +0530

    Initial commit

---
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './ContactHeader.module.css'
+
+const ContactHeader = () => {
+  return (
+    <div className={`${styles.contact_section}`}>
+      <h1>CONTACT US</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur 
+        adipisicing elit. Alias perferendis laudantium iusto,
+         quibusdam, placeat quos ipsam error accusantium ipsum, 
+         possimus culpa numquam fugit! Molestias voluptas laboriosam 
+         enim perspiciatis nulla unde?</p>
+    </div>
+  )
+}
+
+export default ContactHeader
